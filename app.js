7d414e248c21856410bf6773edcd3ff8e312d23b
IVAN-LENOVO\j2050 <j205073@gmail.com>
2024-10-10 08:18:29 +0800

    pro setting

--- app.js
+++ app.js
@@ -12,44 +12,44 @@ app.use(express.json());
 const port = process.env.PORT || 3978;
 
 
-// let adapter;
-// if (process.env.NODE_ENV === 'production') {
-// //   console.log('Using production configuration');
-// //   const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
-// //   adapter = new CloudAdapter(botFrameworkAuthentication);
+let adapter;
+if (process.env.NODE_ENV === 'production') {
+  console.log('Using production configuration');
+  const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
+  adapter = new CloudAdapter(botFrameworkAuthentication);
 // adapter = new BotFrameworkAdapter({});
-// } else {
-//   console.log('Using development configuration');
-//   adapter = new BotFrameworkAdapter({});
-// }
+} else {
+  console.log('Using development configuration');
+  adapter = new BotFrameworkAdapter({});
+}
 
-// // 添加錯誤處理
-// adapter.onTurnError = async (context, error) => {
-//   console.error(`\n [onTurnError] unhandled error: ${error}`);
-//   await context.sendTraceActivity(
-//     'OnTurnError Trace',
-//     `${error}`,
-//     'https://www.botframework.com/schemas/error',
-//     'TurnError'
-//   );
-//   await context.sendActivity('The bot encountered an error or bug.');
-// };
+// 添加錯誤處理
+adapter.onTurnError = async (context, error) => {
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
+  await context.sendTraceActivity(
+    'OnTurnError Trace',
+    `${error}`,
+    'https://www.botframework.com/schemas/error',
+    'TurnError'
+  );
+  await context.sendActivity('The bot encountered an error or bug.');
+};
 
-// const bot = new TeamsBot();
+const bot = new TeamsBot();
 
-// app.post('/api/messages', async (req, res) => {
-//   console.log('Received request:', JSON.stringify(req.body, null, 2));
-//   try {
-//     await adapter.process(req, res, async (context) => {
-//       console.log('Processing activity:', context.activity.type);
-//       await bot.run(context);
-//     });
-//   } catch (error) {
-//     console.error('Error processing request:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-//
+app.post('/api/messages', async (req, res) => {
+  console.log('Received request:', JSON.stringify(req.body, null, 2));
+  try {
+    await adapter.process(req, res, async (context) => {
+      console.log('Processing activity:', context.activity.type);
+      await bot.run(context);
+    });
+  } catch (error) {
+    console.error('Error processing request:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.get('/ping', (req, res) => {
   res.status(200).send({
 env:process.env.NODE_ENV,
